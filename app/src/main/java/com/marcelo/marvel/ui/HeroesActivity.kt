096f45770186c3d01c83bbbe693c52652dbb9aa1
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-11-04 15:37:51 -0300

    UI Components  Main Heroes Done

--- app/src/main/java/com/marcelo/marvel/ui/HeroesActivity.kt
+++ app/src/main/java/com/marcelo/marvel/ui/HeroesActivity.kt
@@ -1,11 +1,12 @@
-package com.marcelo.marvel
+package com.marcelo.marvel.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marcelo.marvel.R
 
-class MainActivity : AppCompatActivity() {
+class HeroesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.list_itens_heroes)
     }
 }
