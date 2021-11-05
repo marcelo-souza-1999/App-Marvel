044d2ed622786f262249b2e8170f16acbc04a670
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-11-05 16:18:00 -0300

    Configure Adapter

---
+++ app/src/main/java/com/marcelo/marvel/ui/adapter/HeroesAdapter.kt
@@ -0,0 +1,53 @@
+package com.marcelo.marvel.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.marcelo.marvel.R
+import com.marcelo.marvel.databinding.ListItensHeroesBinding
+import com.marcelo.marvel.models.Heroes
+
+class HeroesAdapter(private val heroes: List<Heroes>) :
+    RecyclerView.Adapter<HeroesAdapter.HeroesViewHolder>() {
+
+    private val heroesList = ArrayList<Heroes>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroesViewHolder {
+
+        val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
+        val binding: ListItensHeroesBinding = DataBindingUtil.inflate(layoutInflater, R.layout.list_itens_heroes, parent, false)
+
+        return HeroesViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HeroesViewHolder, position: Int) {
+        holder.bind(heroes[position])
+    }
+
+    override fun getItemCount(): Int {
+        return heroes.size
+    }
+
+    fun setList(heroes: List<Heroes>) {
+        heroesList.clear()
+        heroesList.addAll(heroes)
+    }
+
+    class HeroesViewHolder(private val binding: ListItensHeroesBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(heroes: Heroes) {
+
+            binding.txtNameHeroes.text = heroes.name
+            binding.txtDescriptionHeroes.text = heroes.description
+
+            Glide
+                .with(binding.root)
+                .load(heroes.thumbnail.path + heroes.thumbnail.extension)
+                .centerCrop()
+                .into(binding.imageViewHeroes)
+        }
+    }
+
+}
